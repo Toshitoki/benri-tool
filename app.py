7024854f7282639bbf94d943d73e88b3b21ac4e7
Toshitoki <y220018@mail.ryukoku.ac.jp>
2024-11-28 16:21:58 +0900

    UI作成

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import flet as ft
 print("hello world")
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    tb1 = ft.TextField(label="Enter product name/Model")
+
+    page.add(tb1)
+
+ft.app(main)
